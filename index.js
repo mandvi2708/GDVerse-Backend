959c92cd06d9c152f7c2c635b9b5a6254f194843
mandvi singh <mandvisingh0827@gmail.com>
2025-09-30 00:54:11 +0530

    M1

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
